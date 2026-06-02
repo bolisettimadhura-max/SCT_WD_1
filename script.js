1e541c637455afc6382919aa15a99e6cd1f514cd
bolisettimadhura-max <bolisettimadhura@gmail.com>
2026-06-02 17:32:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", function(){
+
+    let navbar = document.getElementById("navbar");
+
+    if(window.scrollY > 50){
+        navbar.style.background = "blue";
+    }
+    else{
+        navbar.style.background = "black";
+    }
+
+});
